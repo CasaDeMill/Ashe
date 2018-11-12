61f25cb69cb2b9b73ba5e5c0897a6f8da5730e9b
Martin Lindgren <artinmall96@gmail.com>
2018-11-11 22:23:52 -0500

    förbättring

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import './App.css';
 import Countdown from './Countdown.js';
 
 class App extends Component {
+
+  componentDidMount(){
+    document.title = "Ashenmall - 2021"
+  }
+
   render() {
-    const currentDate = new Date();
     return (
       <div className="App">
         <Countdown date={`2021-06-13T00:00:00`} />
         <div className="Video">
-        <iframe width="853" height="480" src="https://www.youtube.com/embed/OR-ZccJyFhM?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe width="853" height="480" src="https://www.youtube.com/embed/OR-ZccJyFhM?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen" allowfullscreen></iframe>
         </div>
-      </div>
+      </div> 
     );
   }
 }
