5040d138a5ce0e877a9052209bdebe58da9ea2bf
Martin Lindgren <artinmall96@gmail.com>
2018-11-11 21:07:58 -0500

    Updates

---
+++ precache-manifest.8e41a1fcf4ddb732e6c1ec6c82333e75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ba2d39432e54fc68350",
+    "url": "/Ashe/static/js/runtime~main.0ba2d394.js"
+  },
+  {
+    "revision": "81871f4cab6b6a8e1106",
+    "url": "/Ashe/static/js/main.81871f4c.chunk.js"
+  },
+  {
+    "revision": "0cdd6f184db929cffcf5",
+    "url": "/Ashe/static/js/1.0cdd6f18.chunk.js"
+  },
+  {
+    "revision": "81871f4cab6b6a8e1106",
+    "url": "/Ashe/static/css/main.43e771d4.chunk.css"
+  },
+  {
+    "revision": "ed501ab357ed78b891637d985bb3a442",
+    "url": "/Ashe/index.html"
+  }
+];
