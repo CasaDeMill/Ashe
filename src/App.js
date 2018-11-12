35389975cc86cfab9297e09f0c1002e1e859549c
Martin Lindgren <artinmall96@gmail.com>
2018-11-11 21:55:42 -0500

    nästan

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Countdown from './Countdown.js';
 
 class App extends Component {
   render() {
+    const currentDate = new Date();
     return (
       <div className="App">
-        <iframe width="1280" height="720" src="https://www.youtube.com/embed/OR-ZccJyFhM?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <Countdown date={`2021-06-13T00:00:00`} />
+        <div className="Video">
+        <iframe width="853" height="480" src="https://www.youtube.com/embed/OR-ZccJyFhM?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
       </div>
     );
   }
