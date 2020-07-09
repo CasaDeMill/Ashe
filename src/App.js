b2bf95b3153d46f938fa9c1e5f1e4cf9b12eb1f9
Martin Lindgren <artinmall96@gmail.com>
2020-07-09 22:56:36 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
       <div className="App">
         <Countdown date={`2021-06-13T00:00:00`} />
         <div className="Video">
-          <iframe width="853" height="480" src="https://www.youtube.com/embed/OR-ZccJyFhM?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen" allowfullscreen></iframe>
+          <iframe title='iframe' width="853" height="480" src="https://www.youtube.com/embed/OR-ZccJyFhM?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen" allowfullscreen></iframe>
         </div>
       </div> 
     );
