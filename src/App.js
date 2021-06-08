28e91710e12886246202009b57be51582837e332
Martin Lindgren <artinmall96@gmail.com>
2021-06-08 12:38:52 +0200

    Update time

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ class App extends Component {
       <div className="App">
         <audio ref="audio_tag" src={ heartbeat } autoPlay loop/>
         <h1>One more month...</h1>
-        <Countdown date={`2021-06-13T00:00:00`} />
+        <h1>13th of June 2021 - 23:00</h1>
+        <Countdown date={`2021-06-13T23:00:00`} />
         <div className="Video">
           <iframe title='iframe' width="853" height="480" src="https://www.youtube.com/embed/OR-ZccJyFhM?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen" allowfullscreen></iframe>
         </div>
